77a3856b3b4e1114301852d5906b73f1a093ffa4
Patrick_Pu <pxlxinzhao@gmail.com>
2015-10-27 23:30:28 -0400

    miao

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,41 @@
-var baseUrl = "http://patrickpu.meteor.com";
-//var baseUrl = "http://localhost:3000";
+//var baseUrl = "http://patrickpu.meteor.com";
+var baseUrl = "http://localhost:3000";
 
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http) {
+.controller('DashCtrl', function($scope, $http, $state) {
     refresh();
 
-    $scope.refresh = refresh();
+    $scope.refresh = refresh;
+    $scope.$state = $state;
 
     function refresh(){
       $http.get(baseUrl + "/post")
         .success(function(data) {
           console.log(data);
           $scope.posts = data;
-        });
+        }).finally(function() {
+          // Stop the ion-refresher from spinning
+          $scope.$broadcast('scroll.refreshComplete');
+        });;
     }
   })
 
-.controller('ComposeCtrl', function($scope, $http){
+.controller('ComposeCtrl', function($scope, $http, $state, $stateParams){
     $scope.newPost = {
       name: "Patrick"
     };
     $scope.post = function(){
       $scope.newPost.createdTime = new Date().toISOString();
       console.log($scope.newPost);
-      $http.post(baseUrl + '/post/create', $scope.newPost).then(function(data){
+      $http({
+        method: 'POST',
+        url: baseUrl + '/post/create',
+        data: serializeData($scope.newPost),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function(data){
         console.log('Success: ', data);
+        $state.transitionTo('tab.dash', null, {reload: true, notify:true});
       })
     }
   })
@@ -54,3 +64,26 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
+
+
+
+function serializeData( data ) {
+  // If this is not an object, defer to native stringification.
+  if ( ! angular.isObject( data ) ) {
+    return( ( data == null ) ? "" : data.toString() );
+  }
+  var buffer = [];
+  // Serialize each key in the object.
+  for ( var name in data ) {
+    if ( ! data.hasOwnProperty( name ) ) {
+      continue;
+    }
+    var value = data[ name ];
+    buffer.push(
+      encodeURIComponent( name ) + "=" + encodeURIComponent( ( value == null ) ? "" : value )
+    );
+  }
+  // Serialize the buffer and clean it up for transportation.
+  var source = buffer.join( "&" ).replace( /%20/g, "+" );
+  return( source );
+}
